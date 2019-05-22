1b2a2db9b86e8933fdc899546840294cdb3812d7
vaytsel <pvaytsel@synergy.ru>
2019-05-22 15:00:12 +0300

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+react-cat-food-app
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    )     
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
