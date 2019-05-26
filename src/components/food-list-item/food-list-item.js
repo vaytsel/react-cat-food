880428725776848de71f5f42ee83f20470fea953
vaytsel <pvaytsel@synergy.ru>
2019-05-26 19:14:40 +0300

    mass

--- src/components/food-list-item/food-list-item.js
+++ src/components/food-list-item/food-list-item.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './food-list-item.scss';
 
-function FoodListItem() {
-    return (
+function FoodListItem(props) {
+
+    function declOfNum(number, titles) {
+        let cases = [2, 0, 1, 1, 1, 2];
+        return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+    }
+
+    const mouseCount = 1;
+    const mouseText = `${mouseCount !== 1 ? mouseCount : ''} ${declOfNum(mouseCount, ['мышь', 'мыши', 'мышей'])} в подарок`;
+
+    return (  
         <div className="food-list-item">
             <div className="food-list-item__bevel"></div>
             <div className="food-list-item__main">                
@@ -11,9 +20,12 @@ function FoodListItem() {
                 <div className="food-list-item__with">с фуа-гра</div>
                 <div className="food-list-item__sum">
                     <div className="food-list-item__portion"><b>10</b> порций</div>
-                    <div className="food-list-item__gift">мышь в подарок</div>
+                    < div className = "food-list-item__gift">{mouseText}</div>
                 </div>
                 <div className="food-list-item__weight">0,5 <span>кг</span></div>
+            </div>
+            <div className="food-list-item__desc text-center">
+                Чего сидишь? Порадуй котэ, <span className="buy-link">купи</span><span>.</span>
             </div>
         </div>        
     )
--- src/components/food-list/food-list.js
+++ src/components/food-list/food-list.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FoodListItem from './../food-list-item/food-list-item';
 import './food-list.scss';
 
 function FoodList() {
+
+    const [items, setItems] = useState([
+        { with: 'c фуа-гра', portion: 10, mouse: 1, weight: 0.5},
+        { with: 'c рыбой', portion: 40, mouse: 2, weight: 2},
+        { with: 'c курой', portion: 100, mouse: 5, weight: 5}
+    ])
+
     return (
         <div className="food-list">
             <FoodListItem />
