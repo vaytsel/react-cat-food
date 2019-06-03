abb29661c6d116c74c48cc97a242fbcb153fcb7f
vaytsel <pvaytsel@synergy.ru>
2019-06-03 15:18:03 +0300

    Updates

---
+++ precache-manifest.35b34a237b8ebc5268e00d26425cddbe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955dfff900b57317ceedd1016aa4d0b1",
+    "url": "/react-cat-food/index.html"
+  },
+  {
+    "revision": "10c52e2e1996a034441e",
+    "url": "/react-cat-food/static/css/main.e9881bda.chunk.css"
+  },
+  {
+    "revision": "c84d388d86dc489e6b8b",
+    "url": "/react-cat-food/static/js/2.46ce2592.chunk.js"
+  },
+  {
+    "revision": "10c52e2e1996a034441e",
+    "url": "/react-cat-food/static/js/main.d28f1a01.chunk.js"
+  },
+  {
+    "revision": "a0ce01f0c8413bb14f63",
+    "url": "/react-cat-food/static/js/runtime~main.175ce37b.js"
+  },
+  {
+    "revision": "15dc34b21907dad506e14a5ecbba73d3",
+    "url": "/react-cat-food/static/media/Exo2.0-Thin.15dc34b2.woff2"
+  },
+  {
+    "revision": "a1c5c8b5c89f0da76efd2a04bf522c16",
+    "url": "/react-cat-food/static/media/Exo2.0-Thin.a1c5c8b5.woff"
+  },
+  {
+    "revision": "02eb6c01b59413814383d815e99c6eca",
+    "url": "/react-cat-food/static/media/bg.02eb6c01.jpg"
+  },
+  {
+    "revision": "53d463d25de948e7ce3c88b5b048357e",
+    "url": "/react-cat-food/static/media/cat.53d463d2.png"
+  }
+]);
